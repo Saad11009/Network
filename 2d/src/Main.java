6d782931742d3e079d16bb177e4d21991ca98f7e
Saad1009 <115893922+Saad11009@users.noreply.github.com>
2024-05-05 13:43:07 +0100

    Add files via upload

--- 2d/src/Main.java
+++ 2d/src/Main.java
@@ -1,9 +1,4 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class Main {public static void main(String[] args) throws IOException {
     FullNode node = new FullNode();
